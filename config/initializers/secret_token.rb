6f58bd9d24a3f4197dbcdb389d10c37489e5fd45
Grant Custer <GrantCuster@gmail.com>
2012-01-22 17:00:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImGoing::Application.config.secret_token = '14ef1d495e347928e0033957b72cf717eea6a19909f30eac0cc15af8d1f55bfb2a1fd06c83ef7bbcc755c53ab219bb98d29effb5e24cdfb733320c1bc6a1bde2'
